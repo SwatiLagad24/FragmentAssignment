2ee0e0e0f66e34b6c581a7f601939d32b0790008
SwatiLagad24 <swatilagad24@gmail.com>
2022-04-27 23:06:56 +0530

    FragmentAssignment

--- app/src/main/java/com/example/fragment_assignment/MainActivity.kt
+++ app/src/main/java/com/example/fragment_assignment/MainActivity.kt
@@ -5,44 +5,22 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import com.example.fragment_assignment.databinding.ActivityMainBinding
+
+
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding : ActivityMainBinding
+
+    private lateinit var loginFragment1 : loginFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       /* var edtUserName = findViewById(R.id.edtUserName) as EditText
-        var edtPassword = findViewById(R.id.edtPassword) as EditText
-        var btnReset = findViewById(R.id.btnReset) as Button
-        var btnSubmit = findViewById(R.id.btnSubmit) as Button
 
-*/
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        init()
+    }
 
-       binding.btnReset.setOnClickListener {
-            // clearing user_name and password edit text views on reset button click
-            binding.edtUserName.setText("")
-            binding.edtPassword.setText("")
-        }
-
-        // set on-click listener
-        binding.btnSubmit.setOnClickListener {
-           // val user_name = edtUserName.text;
-            //val password = edtPassword.text;
-            if(binding.edtUserName.getText().toString().equals("swati")&& binding.edtPassword.getText().toString().equals("1234"))
-            Toast.makeText(this@MainActivity, "Login Succesfull", Toast.LENGTH_LONG).show()
-            else {
-                Toast.makeText(this@MainActivity, "Login Failed Try again", Toast.LENGTH_LONG)
-                    .show()
-                binding.edtUserName.setText("")
-                binding.edtPassword.setText("")
-            }
-
-
-        }
+    private fun init(){
+        //setContentView(R.layout.login_ayout)
+        loginFragment1 = supportFragmentManager.findFragmentById(R.id.loginFragment1) as loginFragment
     }
 }
-
-
---
+++ app/src/main/java/com/example/fragment_assignment/loginFragment.kt
@@ -0,0 +1,44 @@
+package com.example.fragment_assignment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.fragment_assignment.databinding.LoginAyoutBinding
+
+class loginFragment:Fragment (){
+
+        private lateinit var binding : LoginAyoutBinding
+
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+        binding= LoginAyoutBinding.inflate(layoutInflater)
+
+
+        binding.btnGoogle.setOnClickListener {
+            Toast.makeText(context, "Loging In With Google", Toast.LENGTH_LONG).show()
+        }
+
+        binding.btnFacebook.setOnClickListener {
+            Toast.makeText(context, "Loging In With Facebook", Toast.LENGTH_LONG).show()
+        }
+
+        binding.btnGit.setOnClickListener {
+            Toast.makeText(context, "Loging In With GitHub", Toast.LENGTH_LONG).show()
+        }
+
+        binding.btnTwitter.setOnClickListener {
+            Toast.makeText(context, "Loging In With Twitter", Toast.LENGTH_LONG).show()
+        }
+
+        return binding.root
+    }
+}
